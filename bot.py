81a754f8454fc2a2015ac2dd80ea6bbe83154963
faheem <faheem@faheempatel.com>
2013-06-07 07:40:19 +0100

    Add can't find rating tweet

--- bot.py
+++ bot.py
@@ -20,23 +20,24 @@ class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         try: 
             request = data['text'].lower()
-            username = str("@" + data["user"]["screen_name"])
+            username = "@" + data["user"]["screen_name"]
 
             if re.match(movie_regex, request):
                 movie = re.sub(movie_regex, '', request)
-                title, rating = helpers.rate(movie)
-                response = "%s %s is rated %s/100" % (username, title, rating)
-                t.update_status(status = response)
+                result = helpers.rate(movie)
+                if result:
+                    title, rating = result
+                    response = "%s %s is rated %s/100" % (username, title, rating)
+                    t.update_status(status = response)
+                else:
+                    response = "%s Can't find a rating for %s." % (username, movie)
+                    t.update_status(status = response)
 
             elif re.match(weather_regex, request):
                 location = re.sub(weather_regex, '', request)
                 report = helpers.weather(location)
 
-                location_name = report[0]
-                min_today = report[1]
-                max_today = report[2]
-                summary = report[3]
-
+                location_name, min_today, max_today, summary = report
                 info = (username, location_name, min_today, max_today, summary)
 
                 response = u"%s %s\nMin: %.0f\u2103 Max: %.0f\u2103\n%s" % info

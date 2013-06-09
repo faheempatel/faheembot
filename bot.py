fc1e830b9c4833566fc29a5b36395c913b3ebe4b
faheem <faheem@faheempatel.com>
2013-06-09 01:03:52 +0100

    Start refactoring

--- bot.py
+++ bot.py
@@ -9,32 +9,54 @@ SECRET = config.CONSUMER_SECRET
 OAUTH_TOKEN = config.OAUTH_TOKEN
 OAUTH_SECRET = config.OAUTH_SECRET
 
-t = Twython(KEY, SECRET, OAUTH_TOKEN, OAUTH_SECRET)    
-
-movie_regex = r"@\w+ rate "
-weather_regex = r"@\w+ weather for "
-laugh_regex = r"@\w+ make me laugh"
-
 def tweet_rating(tweet_id, username, title, rating):
+    """Replies with a movie rating"""
+
     response = "%s %s is rated %s/100" % (username, title, rating)
-    t.update_status(status = response, in_reply_to_status_id = tweet_id)
+    t.update_status(
+                        status = response,
+                        in_reply_to_status_id = tweet_id
+                   )
 
 def tweet_weather(tweet_id, username, report):
+    """Replies with the weather report"""
+
     location_name, min_today, max_today, summary = report
-    info = (username, location_name, min_today, max_today, summary)
+
+    info = (
+                username,
+                location_name,
+                min_today,
+                max_today,
+                summary
+           )
 
     response = u"%s %s\nMin: %.0f\u2103 Max: %.0f\u2103\n%s" % info
-    t.update_status(status = response, in_reply_to_status_id = tweet_id)
+    t.update_status(
+                        status = response,
+                        in_reply_to_status_id = tweet_id
+                   )
 
 def tweet_link(tweet_id, username, link):
+    """Replies with a link"""
+
     response = "%s %s" % (username, link)
-    t.update_status(status = response, in_reply_to_status_id = tweet_id)
+    t.update_status(
+                        status = response,
+                        in_reply_to_status_id = tweet_id
+                   )
 
 class MyStreamer(TwythonStreamer):
 
     def on_success(self, data):
+        """Data Handler for the Twitter Stream"""
+
         if 'text' not in data:
             return
+
+        movie_regex = r"@\w+ rate "
+        weather_regex = r"@\w+ weather for "
+        laugh_regex = r"@\w+ make me laugh"
 
         request = data['text'].lower()
         tweet_id = data['id']
@@ -64,5 +86,6 @@ class MyStreamer(TwythonStreamer):
         t.update_status(status = tweet)
         print status_code
 
+t = Twython(KEY, SECRET, OAUTH_TOKEN, OAUTH_SECRET)    
 stream = MyStreamer(KEY, SECRET, OAUTH_TOKEN, OAUTH_SECRET)    
 stream.statuses.filter(track='@faheembot')
